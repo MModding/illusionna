bacc5e44f178f45848431c98144e8d6a4b9e1bf6
FirstMegaGame4 <firstmegagame4@outlook.fr>
2025-10-05 14:07:59 +0200

    Set New Banner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
 import NavigationMenu from "./components/NavigationMenu.vue";
+import AppContent from "./components/AppContent.vue";
 console.log("Illusionna has started!");
 </script>
 
 <template>
   <NavigationMenu></NavigationMenu>
-  <h1 class="title">Welcome to Illusionna!</h1>
+  <AppContent class="app-content"></AppContent>
 </template>
 
 <style scoped>
+.app-content {
+  padding: 25px;
+}
 </style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="title">Welcome to Illusionna!</h1>
+</template>
+
+<style scoped>
+</style>
